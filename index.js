44eada5042037deba585b3c3231c793c602bed4d
Shripal Jain <shripal@thinkstrap.com>
2018-01-03 12:46:00 +0530

    Download File Option Added

--- index.js
+++ index.js
@@ -2,17 +2,22 @@ var express = require('express');
 var fileUpload = require('express-fileupload');
 var app = express();
 var csv=require('csvtojson');
+var jsonfile = require('jsonfile');
 
 app.use(fileUpload());
 
 app.get('/', express.static('public'));
+
+app.get('/download.json', function (req, res) {
+    res.sendFile('files/data.json', { root: __dirname });
+});
 
 app.post('/upload', function(req, res) {
   if (!req.files)
     res.status(400).send('No files were uploaded.');
  
   let sampleFile = req.files.userfile;
- 
+  var file = 'files/data.json';
   sampleFile.mv('sample.csv', function(err) {
     if (err)
       res.status(500).send(err);
@@ -20,6 +25,9 @@ app.post('/upload', function(req, res) {
 	csv()
 		.fromFile('sample.csv')
 		.on('end_parsed',(jsonObj)=>{
+			jsonfile.writeFile(file, jsonObj, function (err) {
+			  console.error(err)
+			});
 			res.json(jsonObj);
 		})
 		.on('done',(error)=>{
